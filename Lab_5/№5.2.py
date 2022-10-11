abb5cc22e3a2a1c142a79ff1f30d639eb85d9a4c
d.raschepkin <drashepkin9@gmail.com>
2022-10-11 15:50:35 +0300

    Lab №5

---
+++ Lab_5/№5.2.py
@@ -0,0 +1,28 @@
+import re
+
+
+string = input()
+
+start = 0
+end = -1
+for i in range(len(string)):
+    if string[i] != ' ':
+        start = i
+        break
+for i in range(1, len(string)):
+    if string[-i] != ' ':
+        end = len(string) - i
+        break
+string = string[start:end + 1]
+string = string[0].upper() + string[1:].lower()
+
+end = -1
+for i in range(1, len(string)):
+    res = re.findall(r"[^a-z ]", string[-i])
+    if res:
+        end = len(string) - i
+    else:
+        break
+string = string[:end] + "."
+
+print(string)
