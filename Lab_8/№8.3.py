de76b391fbdc1fd03201f21d6280d1781c87f50d
Dmitriy Raschepkin <drashepkin9@gmail.com>
2022-10-25 20:29:32 +0300

    Repaired lab_8

--- Lab_8/№8.3.py
+++ Lab_8/№8.3.py
@@ -1,4 +1,13 @@
 data = {'кот': 'cat', 'собака': 'dog', 'дерево': 'tree', 'дом': 'house', 'дорога': 'road', 'лес': 'forest', 'книга': 'book'}
 words = input('').lower().split()
+new_word = 0
+res = ""
 for word in words:
-    print(data[word], end=' ')
+    if word in data:
+        res += data[word] + " "
+    else:
+        new_word += 1
+if new_word:
+    print("К сожалению,", new_word, "слов из введенных вами пока нет в словаре")
+print("Результат:")
+print(res[:-1].capitalize())
